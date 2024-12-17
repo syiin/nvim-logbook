383eb111f878df1b77923466388998a66677fcd6
Syiin <xbio.oo@gmail.com>
2024-12-17 21:44:32 +0800

    normalise filepaths so we don't make ~ directories everywhere

--- lua/logbook/config.lua
+++ lua/logbook/config.lua
@@ -1,22 +1,47 @@
--- lua/logbook/config.lua
 local M = {}
 
+-- Helper function to safely expand and normalize paths
+local function normalize_path(path)
+	-- If path starts with ~, expand it
+	if path:match("^~") then
+		path = vim.fn.expand(path)
+	end
+
+	-- Remove any trailing slashes
+	path = path:gsub("/$", "")
+
+	-- Convert to absolute path
+	if not path:match("^/") then
+		path = vim.fn.fnamemodify(path, ":p")
+	end
+
+	return path
+end
+
 M.defaults = {
-	default_path = vim.fn.expand("~/logbook"),
+	default_path = normalize_path("~/logbook"),
 	date_format = "%Y-%m-%d %H:%M:%S",
 	file_extension = ".md",
 	use_default_keymaps = true,
-	template = nil, -- Optional template for new files
+	template = nil,
 }
 
 M.options = {}
 
 function M.setup(opts)
+	-- If user provides a path, normalize it
+	if opts and opts.default_path then
+		opts.default_path = normalize_path(opts.default_path)
+	end
+
 	M.options = vim.tbl_deep_extend("force", M.defaults, opts or {})
 
 	-- Create default directory if it doesn't exist
 	if vim.fn.isdirectory(M.options.default_path) == 0 then
-		vim.fn.mkdir(M.options.default_path, "p")
+		local success, err = pcall(vim.fn.mkdir, M.options.default_path, "p")
+		if not success then
+			vim.notify("Failed to create logbook directory: " .. err, vim.log.levels.ERROR)
+		end
 	end
 end
 
--- plugin/logbook.lua
+++ plugin/logbook.lua
@@ -1,77 +1,76 @@
--- ~/.config/nvim/lua/plugins/logbook.lua
 return {
-  'syiin/nvim-logbook', -- replace with your preferred plugin name
-  lazy = false,
-  config = function()
-    local logbook = {}
+	"syiin/nvim-logbook", -- replace with your preferred plugin name
+	lazy = false,
+	config = function()
+		local logbook = {}
 
-    -- Configuration with defaults
-    logbook.config = {
-      default_path = vim.fn.expand '~/logbook',
-      date_format = '%Y-%m-%d %H:%M:%S',
-      file_extension = '.md',
-    }
+		-- Configuration with defaults
+		logbook.config = {
+			default_path = vim.fn.expand("~/logbook"),
+			date_format = "%Y-%m-%d %H:%M:%S",
+			file_extension = ".md",
+		}
 
-    -- Function to open or create a logbook file
-    local function open_logbook(name)
-      local filename
-      if name and name ~= '' then
-        filename = logbook.config.default_path .. '/' .. name .. logbook.config.file_extension
-      else
-        -- Use today's date as filename if none specified
-        filename = logbook.config.default_path .. '/' .. os.date '%Y-%m-%d' .. logbook.config.file_extension
-      end
+		-- Function to open or create a logbook file
+		local function open_logbook(name)
+			local filename
+			if name and name ~= "" then
+				filename = logbook.config.default_path .. "/" .. name .. logbook.config.file_extension
+			else
+				-- Use today's date as filename if none specified
+				filename = logbook.config.default_path .. "/" .. os.date("%Y-%m-%d") .. logbook.config.file_extension
+			end
 
-      -- Create file if it doesn't exist
-      if vim.fn.filereadable(filename) == 0 then
-        local file = io.open(filename, 'w')
-        if file then
-          file:write('# Logbook: ' .. vim.fn.fnamemodify(filename, ':t:r') .. '\n\n')
-          file:close()
-        end
-      end
+			-- Create file if it doesn't exist
+			if vim.fn.filereadable(filename) == 0 then
+				local file = io.open(filename, "w")
+				if file then
+					file:write("# Logbook: " .. vim.fn.fnamemodify(filename, ":t:r") .. "\n\n")
+					file:close()
+				end
+			end
 
-      -- Open the file
-      vim.cmd('edit ' .. vim.fn.fnameescape(filename))
-    end
+			-- Open the file
+			vim.cmd("edit " .. vim.fn.fnameescape(filename))
+		end
 
-    -- Function to insert timestamp at cursor position
-    local function insert_timestamp()
-      local timestamp = os.date(logbook.config.date_format)
-      local pos = vim.api.nvim_win_get_cursor(0)
-      local line = vim.api.nvim_get_current_line()
-      local new_line = line:sub(1, pos[2]) .. timestamp .. line:sub(pos[2] + 1)
-      vim.api.nvim_set_current_line(new_line)
-      vim.api.nvim_win_set_cursor(0, { pos[1], pos[2] + #timestamp })
-    end
+		-- Function to insert timestamp at cursor position
+		local function insert_timestamp()
+			local timestamp = os.date(logbook.config.date_format)
+			local pos = vim.api.nvim_win_get_cursor(0)
+			local line = vim.api.nvim_get_current_line()
+			local new_line = line:sub(1, pos[2]) .. timestamp .. line:sub(pos[2] + 1)
+			vim.api.nvim_set_current_line(new_line)
+			vim.api.nvim_win_set_cursor(0, { pos[1], pos[2] + #timestamp })
+		end
 
-    -- Create default directory if it doesn't exist
-    if vim.fn.isdirectory(logbook.config.default_path) == 0 then
-      vim.fn.mkdir(logbook.config.default_path, 'p')
-    end
+		-- Create default directory if it doesn't exist
+		if vim.fn.isdirectory(logbook.config.default_path) == 0 then
+			vim.fn.mkdir(logbook.config.default_path, "p")
+		end
 
-    -- Create user commands
-    vim.api.nvim_create_user_command('LogbookOpen', function(opts)
-      open_logbook(opts.args)
-    end, {
-      nargs = '?',
-      complete = function()
-        return vim.fn.glob(logbook.config.default_path .. '/*' .. logbook.config.file_extension, false, true)
-      end,
-    })
+		-- Create user commands
+		vim.api.nvim_create_user_command("LogbookOpen", function(opts)
+			open_logbook(opts.args)
+		end, {
+			nargs = "?",
+			complete = function()
+				return vim.fn.glob(logbook.config.default_path .. "/*" .. logbook.config.file_extension, false, true)
+			end,
+		})
 
-    vim.api.nvim_create_user_command('LogbookInsertTimestamp', function()
-      insert_timestamp()
-    end, {})
+		vim.api.nvim_create_user_command("LogbookInsertTimestamp", function()
+			insert_timestamp()
+		end, {})
 
-    -- Set up key mappings
-    vim.keymap.set('n', '<leader>ll', ':LogbookOpen<CR>', { noremap = true, silent = true })
-    vim.keymap.set('n', '<leader>lt', ':LogbookInsertTimestamp<CR>', { noremap = true, silent = true })
-  end,
-  opts = {
-    -- Optional: override defaults here
-    -- default_path = '~/documents/logbook',
-    -- date_format = '%Y-%m-%d %H:%M:%S',
-    -- file_extension = '.md'
-  },
+		-- Set up key mappings
+		vim.keymap.set("n", "<leader>ll", ":LogbookOpen<CR>", { noremap = true, silent = true })
+		vim.keymap.set("n", "<leader>lt", ":LogbookInsertTimestamp<CR>", { noremap = true, silent = true })
+	end,
+	opts = {
+		-- Optional: override defaults here
+		-- default_path = '~/documents/logbook',
+		-- date_format = '%Y-%m-%d %H:%M:%S',
+		-- file_extension = '.md'
+	},
 }
